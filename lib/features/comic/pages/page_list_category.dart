8da849961fd82810010ced23e2dfcb32e9bbcf89
tngmait12 <tongmaitruongvu11@gmail.com>
2025-05-06 06:55:22 +0700

    fetch api list category

--- lib/features/comic/pages/page_list_category.dart
+++ lib/features/comic/pages/page_list_category.dart
@@ -1,3 +1,6 @@
+import 'package:comic_app/features/comic/fetch_api/fetch_list_category.dart';
+import 'package:comic_app/features/comic/models/category_comic.dart';
+import 'package:comic_app/my_widget/async_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,8 +9,6 @@ class PageListCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    List<String> categories = ["Truyen moi", "Truyen hay", "Dang phat hanh","Top Thang","Top Luot xem"];
 
     return Scaffold(
       appBar: AppBar(
@@ -20,33 +21,44 @@ class PageListCategory extends StatelessWidget {
         title: Text("Danh sach the loai", style: TextStyle(color: Colors.white),),
         backgroundColor: Theme.of(context).colorScheme.shadow,
       ),
-      body: ListView.builder(
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            final category = categories[index];
+      body: FutureBuilder(
+          future: fetchListCategoryComic(),
+          builder: (context, snapshot) {
+            return AsyncWidget(
+                snapshot: snapshot,
+                builder: (context, snapshot) {
+                  var categories = snapshot.data! as List<CategoryComic>;
+                  return ListView.builder(
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      final category = categories[index];
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[850], // nền nổi bật hơn shadow
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      blurRadius: 6,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  category,
-                  style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[850], // nền nổi bật hơn shadow
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 6,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          padding: const EdgeInsets.all(16),
+                          child: Text(
+                            category.name,
+                            style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+
+                    },
+                  );
+                },
             );
-
           },
       ),
       backgroundColor: Theme.of(context).colorScheme.shadow,
